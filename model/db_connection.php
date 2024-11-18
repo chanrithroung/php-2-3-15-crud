6ac6ca7e2c48a48913b955f8773f06d64533b847
chanrithroung2@gmail.com <chanrithroung2@gmail.com>
2024-11-18 15:16:56 +0700

    crud_d1

---
+++ model/db_connection.php
@@ -0,0 +1,27 @@
+<?php 
+    $HOST        = "127.0.0.1";
+    $DB_USER     = "root";
+    $DB_PASSWORD = "";
+    $DB_NAME     = "php_crud"; 
+
+
+    function db_conection() {
+        global $HOST, $DB_PASSWORD, $DB_USER, $DB_NAME;
+        
+        try {
+            $pdo = new PDO("mysql:host=$HOST", $DB_USER, $DB_PASSWORD);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "CREATE DATABASE IF NOT EXISTS $DB_NAME;";
+            $pdo->exec($sql);
+            
+            echo "Database connect and create successfully!";
+        } catch (PDOException $e) {
+            echo "Have something when wrong : ".$e->getMessage();
+        } 
+    }
+
+
+
+    db_conection();
+
+    
